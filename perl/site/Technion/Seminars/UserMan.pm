ab7de15065981763ccba694466755c5d9af1ccd6
shlomif <shlomif@localhost>
2002-07-29 13:07:58 +0000

    Some bug fixes in the user manager.

--- perl/site/Technion/Seminars/UserMan.pm
+++ perl/site/Technion/Seminars/UserMan.pm
@@ -45,12 +45,22 @@ sub get_admin_level
 
     my $data = $sth->fetchrow_arrayref();
 
+    # If a user with this username was not found -
+    # make him a readonly user.
+    if (!defined($data))
+    {
+        return "readonly";
+    }
+
+    # Check if the password match
     if ($data->[0] eq $password)
     {
+        # The Super_Admin flag determines the level of the user
         return ($data->[1] ? "site" : "club");
     }
     else
     {
+        # The passwords do not match - fall back to read only.
         return "readonly";
     }
 }
