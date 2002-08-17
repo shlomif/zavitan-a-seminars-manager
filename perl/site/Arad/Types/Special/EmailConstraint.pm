5363d919abfd7efc95d6c348229d2c2d28b1ff28
shlomif <shlomif@localhost>
2002-08-17 10:46:09 +0000

    Added the E-mail Constraint.

---
+++ perl/site/Arad/Types/Special/EmailConstraint.pm
@@ -0,0 +1,32 @@
+# Arad::Types::Special::EmailConstraint - an E-mail constraint
+# (for a string)
+#
+# Requires the ckaddr script by Tom Christiansen (available on CPAN)
+
+package Arad::Types::Special::EmailConstraint;
+
+use strict;
+
+
+my $length_param = 'len';
+
+sub check_value
+{
+    my $typeman = shift;
+    my $type_spec = shift;
+    my $type_params = shift;
+    my $value = shift;
+
+    return system("ckaddr", $value);
+}
+
+sub get_type_params
+{
+    return ("email_constraint",
+        {
+            'check_value' => \&check_value ,
+        }
+        );
+}
+
+1;
