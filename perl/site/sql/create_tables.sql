5887f5898e4a3ab2ddce3d76e48c3bcb3d09633a
shlomif <shlomif@localhost>
2002-08-21 07:08:29 +0000

    The up-to-date database.

--- perl/site/sql/create_tables.sql
+++ perl/site/sql/create_tables.sql
@@ -35,6 +35,9 @@ CREATE TABLE clubs (
 # Dumping data for table 'clubs'
 #
 
+INSERT INTO clubs VALUES (1,'haifux','Haifa Linux Club','http://linuxclub.il.eu.org/homepage','A club for discussing Linux');
+INSERT INTO clubs VALUES (8,'clubnet','Club-Net','http://comnet.technion.ac.il/','Discuss Computer Networks.');
+
 #
 # Table structure for table 'permissions'
 #
@@ -50,6 +53,18 @@ CREATE TABLE permissions (
 # Dumping data for table 'permissions'
 #
 
+INSERT INTO permissions VALUES (1,1,0,0);
+INSERT INTO permissions VALUES (2,1,0,0);
+INSERT INTO permissions VALUES (3,1,0,0);
+INSERT INTO permissions VALUES (4,1,1,1);
+INSERT INTO permissions VALUES (5,1,0,0);
+INSERT INTO permissions VALUES (6,8,1,0);
+INSERT INTO permissions VALUES (6,1,0,0);
+INSERT INTO permissions VALUES (1,8,0,0);
+INSERT INTO permissions VALUES (2,8,0,0);
+INSERT INTO permissions VALUES (3,8,0,0);
+INSERT INTO permissions VALUES (4,8,1,1);
+INSERT INTO permissions VALUES (5,8,0,0);
 
 #
 # Table structure for table 'seminars'
@@ -107,6 +122,10 @@ CREATE TABLE users (
 # Dumping data for table 'users'
 #
 
-INSERT INTO users VALUES (1,'Shlomi Fish Esq.','shlomif',1,'Hello','shlomif@vipe.technion.ac.il');
+INSERT INTO users VALUES (1,'Haifa Linux Club Esq.','shlomif',1,'Hello',NULL);
 INSERT INTO users VALUES (2,'Roy Glasberg','roi',1,'roi','roy@roy.org');
+INSERT INTO users VALUES (3,'Yoram Yihyieuuu','yoramy',0,'yoram','yoramy@ee.technion.ac.il');
+INSERT INTO users VALUES (4,'Google','hello',0,'toyo','hello@google.com');
+INSERT INTO users VALUES (5,'Yahoo','yahoo',0,'hojoj','shlomif@vipe.stud.technion.ac.il');
+INSERT INTO users VALUES (6,'pop','pop',0,'pop','pop@t2.technion.ac.il');
 
