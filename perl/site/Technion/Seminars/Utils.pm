eabb813faaee550138eb3867afd4a8374bd1b14a
shlomif <shlomif@localhost>
2002-09-03 12:05:52 +0000

    1. Moved week_days_abbrevs to T::S::Utils
    2. month now displays the days' headlines.

--- perl/site/Technion/Seminars/Utils.pm
+++ perl/site/Technion/Seminars/Utils.pm
@@ -9,6 +9,7 @@ use vars qw(@ISA);
 @ISA=qw(Exporter);
 
 @EXPORT=qw(time_remove_secs);
+@EXPORT_OK=qw(@week_days_abbrevs);
 
 sub time_remove_secs
 {
@@ -17,5 +18,12 @@ sub time_remove_secs
     $time_raw =~ m/^(\d+):(\d+)/;
     return "$1:$2";
 }
+
+use vars qw(@week_day_abbrevs);
+
+BEGIN {
+    @week_days_abbrevs= (qw(Sun Mon Tue Wed Thu Fri Sat));
+}
+
 
 1;
